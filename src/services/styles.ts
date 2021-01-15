b93db120a9843dfd0077988bd1b726277d1fdf8e
SaltyAom <aomkirby123@gmail.com>
2021-01-15 15:52:30 +0700

    :blue_book: feat - use 1-classname

--- src/services/styles.ts
+++ src/services/styles.ts
@@ -1,44 +1,14 @@
 import { setup } from 'twind'
 import { virtualSheet } from 'twind/sheets'
 
+import oneClassName from '1-classname'
+
 const sheet = virtualSheet()
-
-const styleMap: Record<string, string> = {}
-let counter = 0
-
-// 0-51 = a-z & A-Z
-// 52-2755 = aa - az && Aa - ZZ
-const generateStyleHash = (value: string) => {
-    if (styleMap[value]) return styleMap[value]
-
-    let hash = ''
-    let localCounter = counter
-
-    let genDivisor = () => 52 ** hash.length
-    let genDecrement = () => 52 ** (hash.length + 1)
-
-    while (localCounter >= 0) {
-        let charCode = Math.floor(localCounter / genDivisor()) % 52
-
-        localCounter = localCounter - genDecrement()
-
-        // a-z and A-Z otherwise start new className
-        if (charCode < 26) hash += String.fromCharCode(97 + charCode)
-        else hash += String.fromCharCode(65 + charCode - 26)
-    }
-
-    hash = hash.split('').reverse().join('')
-
-    styleMap[value] = hash
-    counter++
-
-    return hash
-}
 
 setup({
     preflight: false,
     mode: 'strict',
-    hash: (string) => generateStyleHash(string),
+    hash: (string) => oneClassName(string),
     darkMode: 'class',
     sheet
 })
